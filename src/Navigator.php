f5ec62e843d7c7f761b9feb916636f6bcf7f9a31
Marc Rubio <marc.rubio@go1.com>
2019-08-10 22:08:01 +1000

    Initial commit

---
+++ src/Navigator.php
@@ -0,0 +1,166 @@
+<?php
+namespace Mrubiosan\LooseSchemaNavigator;
+
+class Navigator
+{
+    private $node;
+
+    private $exists = true;
+
+    private $satisfiesFn;
+
+    public function __construct($node)
+    {
+        $this->node = $node;
+    }
+
+    /**
+     * @param $name
+     * @return self
+     */
+    public function __get($name)
+    {
+        $retval = clone $this;
+        if (!$retval->exists) {
+            return $retval;
+        }
+
+        if (is_string($retval->node)) {
+            [$retval->node, $retval->exists] = $this->jsonDecode($retval->node);
+        }
+
+        if (is_array($retval->node) && array_key_exists($name, $retval->node)) {
+            $node = $retval->node[$name];
+        } elseif (is_object($retval->node) && property_exists($retval->node, $name)) {
+            $node = $retval->node->$name;
+        } else {
+            $retval->exists = false;
+        }
+
+        $retval->node = $node ?? null;
+
+        return $retval;
+    }
+
+    /**
+     * If the callable does not evaluate to true, the default value will returned when getting properties.
+     * @param callable $fn The passed function receives the obtained value, and must return a boolean.
+     * @return self
+     */
+    protected function defaultIfUnsatisfied(callable $fn) : self
+    {
+        $newInstance = clone $this;
+        $newInstance->satisfiesFn = $fn;
+        return $newInstance;
+    }
+
+    /**
+     * If obtained values evaluate to PHP's empty, the default value will be returned instead
+     * @return self
+     */
+    public function defaultIfEmpty() : self
+    {
+        return $this->defaultIfUnsatisfied('boolval');
+    }
+
+    public function string(?string $default = '') : ?string
+    {
+        if ($this->isValid() && is_scalar($this->node)) {
+            return (string) $this->node;
+        }
+
+        return $default;
+    }
+
+    public function int(?int $default = 0) : ?int
+    {
+        if ($this->isValid() && is_scalar($this->node)) {
+            return (int) $this->node;
+        }
+
+        return $default;
+    }
+
+    public function float(?float $default = 0.0) : ?float
+    {
+        if ($this->isValid() && is_scalar($this->node)) {
+            return (float) $this->node;
+        }
+
+        return $default;
+    }
+
+    public function bool(?bool $default = false) : ?bool
+    {
+        if ($this->isValid() && is_scalar($this->node)) {
+            return filter_var($this->node, FILTER_VALIDATE_BOOLEAN);
+        }
+
+        return $default;
+    }
+
+    public function dateTime(\DateTime $default = null) : ?\DateTime
+    {
+        $value = $this->node;
+        if ($this->isValid() && is_scalar($value)) {
+            if (ctype_digit((string)$value)) {
+                $value = '@'.$value;
+            }
+            try {
+                return new \DateTime($value);
+            } catch (\Exception $e) {
+            }
+        }
+
+        return $default;
+    }
+
+    public function array(?array $default = []) : ?array
+    {
+        if ($this->isValid()) {
+            if (is_array($this->node)) {
+                return $this->node;
+            }
+
+            if (is_object($this->node)) {
+                return (array) $this->node;
+            }
+        }
+
+        return $default;
+    }
+
+    /**
+     * @param \stdClass|null $default Returns a stdClass by default
+     * @return \stdClass|null
+     */
+    public function object(\stdClass $default = null) : ?\stdClass
+    {
+        if ($this->isValid()) {
+            if (is_array($this->node)) {
+                return (object) $this->node;
+            }
+
+            if (is_object($this->node)) {
+                return $this->node;
+            }
+        }
+
+        if (func_num_args() < 1) {
+            return new \stdClass();
+        }
+        return $default;
+    }
+
+    private function isValid()
+    {
+        $satisfiesFn = $this->satisfiesFn;
+        return $this->exists && ($satisfiesFn === null || $satisfiesFn($this->node) === true);
+    }
+
+    private function jsonDecode(string $jsonStr) : array
+    {
+        return [json_decode($jsonStr), json_last_error() === JSON_ERROR_NONE];
+    }
+}
+
---
+++ tests/NavigatorTest.php
@@ -0,0 +1,127 @@
+<?php
+namespace Mrubiosan\Navigator\Tests;
+
+use Mrubiosan\LooseSchemaNavigator\Navigator;
+use PHPUnit\Framework\TestCase;
+
+class NavigatorTest extends TestCase
+{
+    public function testItTraverses()
+    {
+        $data = [
+            'foo' => [
+                'bar' => (object) [
+                    'baz' => 'hello'
+                ]
+            ]
+        ];
+
+        $testSubject = new Navigator($data);
+        $this->assertEquals('hello', $testSubject->foo->bar->baz->string());
+    }
+
+    public function testItJsonDecodesNodesWhenTraversing()
+    {
+        $data = [
+            'foo' => [
+                'bar' => json_encode(['baz' => 'hello'])
+            ]
+        ];
+
+        $testSubject = new Navigator($data);
+        $this->assertEquals('hello', $testSubject->foo->bar->baz->string());
+
+        $badData = [
+            'foo' => [
+                'bar' => 'notjson'
+            ]
+        ];
+
+        $testSubject = new Navigator($badData);
+        $this->assertEquals('', $testSubject->foo->bar->baz->string());
+    }
+
+    public function testObjectGivenDefault()
+    {
+        $testSubject = new Navigator([]);
+        $defaultObject = new \stdClass();
+        $this->assertSame($defaultObject, $testSubject->foo->object($defaultObject));
+    }
+
+    /**
+     * @dataProvider typeProvider
+     */
+    public function testTypes($method, $value, $expected)
+    {
+        $data = [
+            'foo' => $value
+        ];
+
+        $testSubject = new Navigator($data);
+        $result = $testSubject->foo->$method();
+        if (is_object($expected)) {
+            $this->assertEquals($expected, $result);
+        } else {
+            $this->assertSame($expected, $result);
+        }
+    }
+
+    public function typeProvider()
+    {
+        return [
+            ['string', 'hello', 'hello'],
+            ['string', 123, '123'],
+            ['string', ['not_a_string','but_an_array'], ''],
+            ['string', null, ''],
+            ['int', '123', 123],
+            ['int', 'notnumeric', 0],
+            ['int', new \stdClass(), 0],
+            ['int', true, 1],
+            ['float', '456.123', 456.123],
+            ['float', 456, 456.0],
+            ['float', 'oops', 0.0],
+            ['float', [], 0.0],
+            ['bool', '1', true],
+            ['bool', 'true', true],
+            ['bool', true, true],
+            ['bool', '0', false],
+            ['bool', '123', false],
+            ['bool', null, false],
+            ['dateTime', 123, new \DateTime("@123")],
+            ['dateTime', '1985-12-24', new \DateTime("1985-12-24")],
+            ['dateTime', 'blergh', null],
+            ['dateTime', [], null],
+            ['array', [1, 2, 3], [1, 2, 3]],
+            ['array', ['a' => 1], ['a' => 1]],
+            ['array', (object) [1, 2, 3], [1, 2, 3]],
+            ['array', 'a string', []],
+            ['object', (object) [1, 2, 3], (object) [1, 2, 3]],
+            ['object', ['a' => 1], (object) ['a' => 1]],
+            ['object', 'a string', new \stdClass()],
+        ];
+    }
+
+    public function testDefaultIfEmpty()
+    {
+        $data = [
+            'foo' => [
+                'bar' => '0'
+            ]
+        ];
+
+        $testSubject = new Navigator($data);
+        $this->assertEquals('myDefault', $testSubject->foo->bar->defaultIfEmpty()->string('myDefault'));
+    }
+
+    public function testNonExistingNodes()
+    {
+        $data = [
+            'foo' => 123,
+            'bar' => 456
+        ];
+        $testSubject = new Navigator($data);
+
+        $this->assertEquals(0, $testSubject->baz->bar->int());
+    }
+}
+
